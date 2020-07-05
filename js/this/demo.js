1126d599e91538f1b29fcb06f61f22cd23b95877
aoilti <1051127659@qq.com>
2020-07-05 20:32:55 +0800

    feat: add study note

---
+++ js/this/demo.js
@@ -0,0 +1,74 @@
+// 硬绑定
+function foo() {
+    console.log(this.a);
+}
+
+var obj = {
+    a: 2
+};
+
+var bar = function() {
+    foo.call(this);
+}
+
+bar(); // 2
+setTimeout(bar, 200);  // 2
+bar.call(window);
+
+
+// API调用的上下文
+function foo(el) {
+    console.log(el, this.id);
+}
+
+var obj = {
+    id: "awesome"
+};
+
+[1, 2, 3].forEach(foo, obj);    // 1 awesome   2 awesome    3 awesome
+
+
+
+// 隐式绑定和显式绑定优先级测试
+function foo() {
+    console.log(this.a);
+}
+
+var obj1 = {
+    a: 2,
+    foo
+}
+
+var obj2 = {
+    a: 3,
+    foo
+}
+
+obj1.foo(); // 2
+obj2.foo(); // 3
+
+obj1.foo.call(obj2);  // 3
+obj2.foo.call(obj1);  // 2
+// 结论： 显式大于隐式
+
+
+// new绑定与隐式绑定优先级测试
+function foo(something) {
+    this.a = something;
+}
+
+var obj1 = {
+    foo
+}
+
+var obj2 = {};
+
+obj1.foo(2);
+console.log(obj1.a);  // 2
+
+obj1.foo.call(obj2, 3);
+console.log(obj2.a);  // 3
+
+var bar = new obj1.foo(4);
+console.log(bar.a);  // 4
+console.log(obj1.a);  // 2
---
+++ js/闭包/module-define.demo.js
@@ -0,0 +1,44 @@
+
+var MyModules = (function Manager() {
+    var modules = {};
+
+    function define(name, deps, impl) {
+      for (var i = 0; i < deps.length; i++) {
+        deps[i] = modules[deps[i]];
+      }
+      modules[name] = impl.apply(impl, deps);
+    }
+
+    function get(name) {
+      return modules[name];
+    }
+
+    return {
+      get,
+      define,
+    };
+  })();
+
+  MyModules.define("bar", [], function () {
+    function hello(who) {
+      return "hello " + who;
+    }
+
+    return {
+      hello,
+    };
+  });
+
+  MyModules.define("foo", ["bar"], function (bar) {
+    function awesome() {
+      console.log(bar.hello("tom"));
+    }
+
+    return { awesome };
+  });
+
+  var bar = MyModules.get("bar");
+  var foo = MyModules.get("foo");
+  console.log(bar.hello("jim"));
+
+  foo.awesome();
