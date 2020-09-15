a23eaa90dacb64f0a283e88385e7a916b41d2c41
aoilti <1051127659@qq.com>
2020-09-15 16:37:43 +0800

    feat(data): 添加常见算法与数据结构

--- js/数据结构与算法/demo.js
+++ js/数据结构与算法/demo.js
@@ -16,19 +16,39 @@ function bubbleSort(arr) {
 bubbleSort([2, 31, 45, 1, -1]);
 
 function quickSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const midIndex = parseInt(arr.length / 2);
+  const midVal = arr[midIndex];
+  const left = [];
+  const right = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if (i === midIndex) {
+      continue;
     }
+    if (item <= midVal) {
+      left.push(item);
+    } else {
+      right.push(item);
+    }
+  }
+
+  return [...quickSort(left), midVal, ...quickSort(right)];
 }
 
+quickSort([2, 1, 4, 6, 82, 3, 7, 9, 1]);
+
 function selectSort(arr) {
-    let tempValue;
-    for (let i = 0; i < arr.length; i++) {
-        let minValue = arr[i];
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[j] < minValue) {
-                minValue = arr[j];
-            }
-        }
+  let tempValue;
+  for (let i = 0; i < arr.length; i++) {
+    let minValue = arr[i];
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] < minValue) {
+        minValue = arr[j];
+      }
     }
+  }
 }
